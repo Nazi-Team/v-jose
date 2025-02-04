fcc12039e179e873ad53cc68c6fc3be027b337f6
Alexito-Hub <alessandrovillogas@outlook.es>
2025-02-04 00:35:21 -0500

    Actualizar el plugin de menú para enviar mensajes interactivos y mejorar la gestión de respuestas en el plugin de YouTube

--- src/plugins/menu.js
+++ src/plugins/menu.js
@@ -34,11 +34,14 @@ export default {
             return imageMessage
         }
 
-        await sock.sendMessage(m.from, {
-            image: {
-                url: "./4k.jpg",
-            },
-            caption: `💨 ${greeting} @${m.sender.split('@')[0]}
+        let msg = generateWAMessageFromContent(
+            m.from,
+            {
+                viewOnceMessage: {
+                    message: {
+                        interactiveMessage: {
+                            body: {
+                                text: `💨 ${greeting} @${m.sender.split('@')[0]}
 *¤* ${lang.motivational[Math.floor(Math.random() * lang.motivational.length)]}
 
 *${lang.menu.m}:* ${db.data.settings[sock.user.jid].private ? lang.public_status : lang.private_status}
@@ -88,9 +91,50 @@ Algunos comandos pueden no estar disponibles por el sistema operativo donde se h
 ⁜ .leave
 ⁜ .private <on/off>
 ⁜ .broadcast <query>`,
-            contextInfo: {
-                mentionedJid: [m.sender]
-            }
-        })
+                            },
+                            header: {
+                                title: _config.bot.name,
+                                hasMediaAttachment: true,
+                                productMessage: {
+                                    product: {
+                                        productImage: await image("./nazi.jpg"),
+                                        productId: "8363525327041213",
+                                        title: convertTimeOut(process.uptime() * 1000),
+                                        description: "created by " + _config.owner.name,
+                                        currencyCode: "MXN",
+                                        priceAmount1000: "250000",
+                                        retailerId: "nazi-team-bot",
+                                        url: "https://github.com",
+                                        productImageCount: 1,
+                                    },
+                                    businessOwnerJid: "573013116003@s.whatsapp.net",
+                                },
+                            },
+                            nativeFlowMessage: {
+                                buttons: [
+                                    {
+                                        name: "quick_reply",
+                                        buttonParamsJson: JSON.stringify({
+                                            display_text: "Owner",
+                                            id: ".owner"
+                                        }),
+                                    },
+                                ],
+                            },
+                            contextInfo: {
+                                mentionedJid: [m.sender],
+                            }
+                        },
+                    },
+                },
+            },
+            { quoted: m },
+        );
+
+        await sock.relayMessage(
+            m.from,
+            msg.message,
+            { messageId: msg.key.id }
+        )
     }
 };
--- src/plugins/youtube/youtube.js
+++ src/plugins/youtube/youtube.js
@@ -2,7 +2,7 @@ import YouTube from "../../scraper/youtube.js";
 
 export default {
     name: 'youtube',
-    params: ['query'],
+    params: ['message'],
     description: 'Busca y descarga videos y audios de YouTube',
     comand: ['youtube', 'yt', 'play'],
     os: true,
@@ -11,34 +11,38 @@ export default {
         const video = videos[0];
 
         sock.sendMessage(m.from, {
-            caption: `*Título:* ${video.title}\n*Duración:* ${video.duration}\n*Canal:* ${video.author}\n*Vistas:* ${video.viewers}\n*Subido:* ${video.published}\n\n_Responde con "audio" para descargar el audio o "video" para descargar el video._`,
+            caption: `*Título:* ${video.title}\n*Duración:* ${video.duration}\n*Canal:* ${video.author}\n*Vistas:* ${video.viewers}\n*Subido:* ${video.published}\n\n_Tiempo limite para responder 5 minutos_\n_Solo el remitente puede responder._`,
+            footer: _config.bot.name,
             image: { url: video.thumbnail },
-        })
+            buttons: [
+                { buttonId: 'audio', buttonText: { displayText: 'Audio' } },
+                { buttonId: 'video', buttonText: { displayText: 'Video' } }
+            ],
+            headerType: 6,
+            viewOnce: true
+        });
 
-        const filter = response => response.key.remoteJid === m.from && response.key.participant === m.sender
+        const filter = response => response.key.remoteJid === m.from && response.key.participant === m.sender;
         const timeout = setTimeout(() => {
-            sock.ev.off('messages.upsert', responseHandler)
-        }, 5 * 60 * 1000)
+            sock.ev.off('messages.upsert', responseHandler);
+        }, 5 * 60 * 1000);
 
         const responseHandler = async response => {
-            if (response.messages[0].message && response.messages[0].message.conversation && filter(response.messages[0])) {
-                clearTimeout(timeout)
-                sock.ev.off('messages.upsert', responseHandler)
+            if (response.messages[0].message && response.messages[0].message.buttonsResponseMessage && filter(response.messages[0])) {
+                clearTimeout(timeout);
+                sock.ev.off('messages.upsert', responseHandler);
 
-                const text = response.messages[0].message.conversation
-                const type = text === 'audio' ? 'audio' : text === 'video' ? 'video' : null
+                const type = response.messages[0].message.buttonsResponseMessage.selectedButtonId === 'audio' ? 'audio' : 'video';
+                const url = `https://api.botcahx.eu.org/api/download/get-YoutubeResult?url=https://youtu.be/${video.id}&type=${type}&xky=zMxPoM%C2%81S`;
 
-                if (type) {
-                    const url = `https://api.botcahx.eu.org/api/download/get-YoutubeResult?url=https://youtu.be/${video.id}&type=${type}&xky=zMxPoM%C2%81S`
-
-                    if (type === 'audio') {
-                        await sock.sendMedia(response.key.remoteJid, url)
-                    } else {
-                        await sock.sendMedia(response.key.remoteJid, url, { caption: video.title })
-                    }
+                if (type === 'audio') {
+                    await sock.sendMedia(m.from, url )
+                } else {
+                    await sock.sendMedia(m.from, url, { caption: video.title })
                 }
             }
-        }
+        };
+
         sock.ev.on('messages.upsert', responseHandler)
     }
 }
