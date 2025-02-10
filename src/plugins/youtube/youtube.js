3c9638a6ca3078c6def1ebbbeaaf089469ca77db
Alexito-Hub <alessandrovillogas@outlook.es>
2025-02-10 02:01:35 -0500

    Modificar lógica de envío de medios: simplificar la determinación del tipo de medio y ajustar la conversión a audio

--- src/plugins/youtube/youtube.js
+++ src/plugins/youtube/youtube.js
@@ -41,7 +41,7 @@ export default {
                 if (type === 'audio') {
                     await sock.sendMedia(m.from, await ytmp3(video.url), { convert: true, type: 'audio' })
                 } else if (type === 'video') {
-                    await sock.sendMedia(m.from, await ytmp4(video.url), { caption: video.title, type: 'video' })
+                    await sock.sendMedia(m.from, await ytmp4(video.url), { caption: video.title })
                 }
             }
         }
