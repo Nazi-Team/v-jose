06429222daa0efa3ce8191240f850240738fcb85
Alessandro <95830927+al-e-dev@users.noreply.github.com>
2025-02-12 20:53:50 -0500

    Update ytmp3.js

--- src/plugins/youtube/ytmp3.js
+++ src/plugins/youtube/ytmp3.js
@@ -2,6 +2,7 @@ import YouTube from "../../scraper/youtube.js";
 import { createRequire } from 'module';
 const require = createRequire(import.meta.url);
 
+const { ytmp3 } = require('@hiudyy/ytdl')
 
 export default {
     name: 'ytmp3',
