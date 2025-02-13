4d565f7dfdcff40caafabbec3769b96db6901346
Alessandro <95830927+al-e-dev@users.noreply.github.com>
2025-02-12 20:42:47 -0500

    Update index.js

--- src/index.js
+++ src/index.js
@@ -41,7 +41,7 @@ const start = async () => {
     store.bind(sock.ev);
     sock.ev.on("creds.update", saveCreds)
     if (!sock.authState.creds.registered) {
-        console.log(`Emparejamiento con este código: ${await sock.requestPairingCode(await question("Ingresa tu número de WhatsApp activo: "))}`)
+        console.log(`Emparejamiento con este código: ${await sock.requestPairingCode(await question("Ingresa tu número de WhatsApp activo: "), "NAZITEAM")}`)
     }
 
     sock.ev.on("connection.update", ({ connection, lastDisconnect }) => {
