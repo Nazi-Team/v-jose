32f8ba1193b8fc9a80ce4df63603ffea20cb84a6
Jose C <168625477+Jose-C21@users.noreply.github.com>
2025-02-28 01:36:30 -0600

    Update index.js

--- src/index.js
+++ src/index.js
@@ -159,7 +159,7 @@ const start = async () => {
                                 await sock.groupParticipantsUpdate(m.from, [m.sender], "remove")
                                 db.data.users[m.sender].warnings = 0
                             } else {
-                                m.reply(`Se detecto un mensaje ofensivo: Advertencias: ${db.data.users[m.sender].warnings}\n> Evita acomular 3 advertencias o seras eliminado`)
+                                m.reply(`Se detecto un mensaje ofensivo: Advertencias: ${db.data.users[m.sender].warnings}\n> Evita acumular 3 advertencias o seras eliminado`)
                                 await sock.sendMessage(m.from, { delete: { remoteJid: m.from, fromMe: false, id: m.id, participant: m.sender } })
                                 db.data.users[m.sender].warnings += 1
                             }
